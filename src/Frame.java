3613a36d6cdc6babd1bdc89c9365c540e2078381
Matt Matriciano <mmatriciano@ewu.edu>
2024-04-10 17:29:02 -0700

    init files

---
+++ src/Frame.java
@@ -0,0 +1,45 @@
+public class Frame {
+    private int pinCount = 0;
+    private boolean dirty = false;
+    private String content;
+
+    public Frame(String content) {
+        this.content = content;
+    }
+
+    public int getPin() {
+        // your code goes here
+        return this.pinCount;
+    }
+
+    public void incPin() {
+        this.pinCount++;
+    }
+
+    public void decPin() {
+        if (pinCount > 0)
+            this.pinCount--;
+    }
+
+    public boolean isDirty() {
+        // your code goes here
+        return this.dirty;
+    }
+
+    public void setDirty(boolean dirty) {
+        this.dirty = dirty;
+    }
+
+    public void displayPage() {
+        System.out.println(content);
+    }
+
+    public void updatePage(String toAppend) {
+        content += "\n" + toAppend;
+        dirty = true;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
